72ea3f0c878078771b72d02dbe54fa036a2736f4
JOTHECABLEGUY <jzdube99@gmail.com>
2024-08-20 11:14:08 -0400

    Simplified to base calculator.

--- ConsoleApplication1/Calculator.cpp
+++ ConsoleApplication1/Calculator.cpp
@@ -1,67 +1,20 @@
-#include "Calculator.h"
-#include <cmath>
-#include <iostream>
-#include <regex>
-using namespace std;
-class Calculator {
 
-public:
-	double Calculate(const string orig_input, double x, char oper, double y) {
-		switch (oper) {
-		case('-'):
-			return x - y;
-		case('+'):
-			return x + y;
-		case('/'):
-			return x / y;
-		case('*'):
-			return x * y;
-		default:
-			if (orig_input.find("**") != string::npos) {
-				vector<double> inputs = extract_numbers_from_expr(orig_input, "**");
-				return pow(inputs[0], inputs[1]);
-			}
-			if (orig_input.find("sqrt") != string::npos) {
-				vector<double> inputs = extract_numbers_from_expr(orig_input, "sqrt");
-				return sqrt(inputs[0]);
-			}
-			if (orig_input.find("log") != string::npos) {
-				vector<double> inputs = extract_numbers_from_expr(orig_input, "log");
-				return log2(inputs[0]) / log2(inputs[1]);
-			}
-			return 0.0;
-		}
-		return 0.0;
-	};
-private:
-	vector<double> extract_numbers_from_expr(const string &expr, const string oper) {
-		regex e;
-		vector<double> ret_vec;
-		if (oper == "**") {
-			regex e("(.*)" + oper + "(.*)");
-		}
-		else { regex e("(" + oper + ")\((.*)\)"); };
-		smatch sm;    // same as std::match_results<const char*> cm;
-		regex_match(expr, sm, e);
-		if (not sm.size()) {
-			return ret_vec;
-		}
-		string match_group = sm[2];
-		vector<string> params = tokenize(match_group, ',');
-		for (string s : params) {
-			ret_vec.push_back(stod(s));
-		}
-		return ret_vec;
-	};
-	vector<string> tokenize(const string s, char del) {
-		int right, left = -1;
-		vector<string> ret_vec;
-		while (right != -1) {
-			left = right + 1;
-			right = s.find(del);
-			string new_str = s.substr(left, right - left);
-			ret_vec.push_back(new_str);
-		}
-		return ret_vec;
-	};
-};
+
+#include "Calculator.h"
+#include <iostream>
+
+double Calculator::Calculate(double x, char oper, double y, std::string s)
+{
+    switch (oper) {
+    case ('+'):
+        return x + y;
+    case('-'):
+        return x - y;
+    case('*'):
+        return x * y;
+    case('/'):
+        return x / y;
+    default:
+        return 0.0;
+    }
+}
--- ConsoleApplication1/Calculator.h
+++ ConsoleApplication1/Calculator.h
@@ -1,14 +1,8 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <vector>
-using namespace std;
 class Calculator
 {
 public:
-	double Calculate(const string orig_input, double x, char oper, double y){};
-private:
-	vector<double> extract_numbers_from_expr(const string &expr, const string oper) {};
-	vector<string> tokenize(const string s, char del) {};
+	double Calculate(double x, char oper, double y, std::string s);
 };
-
